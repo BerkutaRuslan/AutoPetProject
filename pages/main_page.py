0558b720949418b45a15d714e658732be849d2f2
Ruslan <berkutruslan1@gmail.com>
2021-01-15 17:34:26 +0200

    updated demo page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,27 +1,17 @@
-from locators.main_page_locators import GoogleMainPageLocators
+from locators.main_page_locators import DemoMainPageLocators
 
 
-class GoogleMainPage:
+class DemoMainPage:
     def __init__(self, browser):
         self.browser = browser
 
-        self.gmail_url = self.browser.find_element(*GoogleMainPageLocators.GMAIL_URL)
-        self.images_url = self.browser.find_element(*GoogleMainPageLocators.IMAGES_URL)
-        self.search_glass_icon = self.browser.find_element(*GoogleMainPageLocators.SEARCH_GLASS_ICON)
-        self.input_field = self.browser.find_element(*GoogleMainPageLocators.INPUT_FIELD)
-        self.mic_icon = self.browser.find_element(*GoogleMainPageLocators.MIC_ICON)
-        self.google_search = self.browser.find_element(*GoogleMainPageLocators.GOOGLE_SEARCH)
-        self.im_feeling_lucky = self.browser.find_element(*GoogleMainPageLocators.IM_FEELING_LUCKY)
-        self.google_offered_in = self.browser.find_element(*GoogleMainPageLocators.GOOGLE_OFFERED_IN)
-        self.bottom_country = self.browser.find_element(*GoogleMainPageLocators.BOTTOM_COUNTRY)
-        self.bottom_advertising = self.browser.find_element(*GoogleMainPageLocators.BOTTOM_ADVERTISING)
-        self.bottom_business = self.browser.find_element(*GoogleMainPageLocators.BOTTOM_BUSINESS)
-        self.bottom_about = self.browser.find_element(*GoogleMainPageLocators.BOTTOM_ABOUT)
-        self.bottom_how_search_works = self.browser.find_element(*GoogleMainPageLocators.BOTTOM_HOW_SEARCH_WORKS)
-        self.bottom_privacy = self.browser.find_element(*GoogleMainPageLocators.BOTTOM_PRIVACY)
-        self.bottom_terms = self.browser.find_element(*GoogleMainPageLocators.BOTTOM_TERMS)
-        self.bottom_settings = self.browser.find_element(*GoogleMainPageLocators.BOTTOM_SETTINGS)
-
-    def input_search(self, text="Default text"):
-        self.input_field.send_keys(text)
-        self.google_search.click()
+        self.home_icon = self.browser.find_element(*DemoMainPageLocators.HOMEPAGE_ICON)
+        self.homepage_button = self.browser.find_element(*DemoMainPageLocators.HOMEPAGE_BUTTON)
+        self.contact_button = self.browser.find_element(*DemoMainPageLocators.CONTACT_BUTTON)
+        self.about_us_button = self.browser.find_element(*DemoMainPageLocators.ABOUT_US_BOTTON)
+        self.cart_button = self.browser.find_element(*DemoMainPageLocators.CART_BUTTON)
+        self.login_button = self.browser.find_element(*DemoMainPageLocators.LOGIN_BUTTON)
+        self.sign_up_button = self.browser.find_element(*DemoMainPageLocators.SIGN_UP_BUTTON)
+        self.previous_advertising_image_button = self.browser\
+            .find_element(*DemoMainPageLocators.PREVIOUS_ADVERTISING_IMAGE)
+        self.next_advertising_image_button = self.browser.find_element(*DemoMainPageLocators.NEXT_ADVERTISING_IMAGE)
