70d57b1bd16d17f364f83c8c21d5b7a11d3b2547
Ruslan <berkutruslan1@gmail.com>
2021-01-15 13:35:29 +0200

    updated locators

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -3,27 +3,6 @@ from selenium.webdriver.common.by import By
 
 
 @dataclass
-class GoogleMainPageLocators:
-    GMAIL_URL = (By.XPATH, "//a[contains(text(),'Gmail')]")
-    IMAGES_URL = (By.XPATH, "//a[contains(text(),'Images')]")
-    GOOGLE_LOGO = (By.XPATH, "//img[@id='hplogo']")
-    SEARCH_GLASS_ICON = (By.XPATH, "//body/div[@id='viewport']/div[@id='searchform']/form[@id='tsf']/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/span[1]/*[1]")  # noqa
-    INPUT_FIELD = (By.XPATH, "//body/div[@id='viewport']/div[@id='searchform']/form[@id='tsf']/div[2]/div[1]/div[1]/div[1]/div[2]/input[1]")  # noqa
-    MIC_ICON = (By.XPATH, "//body/div[@id='viewport']/div[@id='searchform']/form[@id='tsf']/div[2]/div[1]/div[1]/div[1]/div[3]/div[2]/*[1]")  # noqa
-    GOOGLE_SEARCH = (By.XPATH, "//body/div[@id='viewport']/div[@id='searchform']/form[@id='tsf']/div[2]/div[1]/div[3]/center[1]/input[1]")
-    IM_FEELING_LUCKY = (By.XPATH, "//body/div[@id='viewport']/div[@id='searchform']/form[@id='tsf']/div[2]/div[1]/div[3]/center[1]/input[2]")
-    GOOGLE_OFFERED_IN = (By.XPATH, "//div[@id='SIvCob']")
-    BOTTOM_COUNTRY = (By.XPATH, "//span[contains(text(),'Ukraine')]")
-    BOTTOM_ADVERTISING = (By.XPATH, "//a[contains(text(),'Advertising')]")
-    BOTTOM_BUSINESS = (By.XPATH, "//a[contains(text(),'Business')]")
-    BOTTOM_ABOUT = (By.XPATH, "//a[contains(text(),'About')]")
-    BOTTOM_HOW_SEARCH_WORKS = (By.XPATH, "//a[contains(text(),'How Search works')]")
-    BOTTOM_PRIVACY = (By.XPATH, "//body/div[@id='viewport']/div[@id='main']/div[@id='footer']/div[@id='fbarcnt']/div[@id='footcnt']/div[@id='fbar']/div[1]/span[1]/a[1]")
-    BOTTOM_TERMS = (By.XPATH, "//body/div[@id='viewport']/div[@id='main']/div[@id='footer']/div[@id='fbarcnt']/div[@id='footcnt']/div[@id='fbar']/div[1]/span[1]/a[2]")
-    BOTTOM_SETTINGS = (By.XPATH, "//a[@id='fsettl']")
-
-
-@dataclass
 class DemoMainPageLocators:
     HOMEPAGE_ICON = (By.XPATH, "//a[@id='nava']")
     HOMEPAGE_BUTTON = (By.XPATH, "//a[.='Home (current)']")
@@ -47,11 +26,11 @@ class DemoCategoryLocators:
 
 @dataclass
 class PhoneDemoProductLocators:
-    SAMSUNG_GALAXY_S6 = (By.XPATH, "//div[@id='tbodyid']/div[1]//img[@src='imgs/galaxy_s6.jpg']")
-    NOKIA_LUMIA_1520 = (By.XPATH, "//img[@src='imgs/Lumia_1520.jpg']")
-    NEXUS_6 = (By.XPATH, "//img[@src='imgs/Nexus_6.jpg']")
+    SAMSUNG_GALAXY_S6 = (By.XPATH, "//a[.='Samsung galaxy s6']")
+    NOKIA_LUMIA_1520 = (By.XPATH, "//a[.='Nokia lumia 1520']']")
+    NEXUS_6 = (By.XPATH, "//a[.='Nexus 6']']")
     SAMSUNG_GALAXY_S7 = (By.XPATH, "//a[.='Samsung galaxy s7']")
-    IPHONE_6_32GB = (By.XPATH, "//img[@src='imgs/iphone_6.jpg']")
+    IPHONE_6_32GB = (By.XPATH, "//a[.='Iphone 6 32gb']")
     SONY_XPERIA = (By.XPATH, "//a[.='Sony xperia z5']")
     HTC_ONE_M9 = (By.XPATH, "//a[.='HTC One M9']")
 
