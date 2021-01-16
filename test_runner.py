f77723777fa4527f5802ec0aeb0a96eee2416bf7
Ruslan <berkutruslan1@gmail.com>
2021-01-16 21:28:24 +0200

    added test for a phone

--- test_runner.py
+++ test_runner.py
@@ -1,6 +1,7 @@
 from pages.main_page import DemoMainPage
 
 
-def test_basic_search(browser):
-    DemoMainPage(browser)
-    print(browser.title)
+def test_product_samsung_g_6_page(browser):
+    DemoMainPage(browser).phones_SAMSUNG_GALAXY_S6.click()
+    assert browser.current_url == 'https://www.demoblaze.com/prod.html?idp_=1'
+
