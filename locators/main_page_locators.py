4e9c233693fbeb404fe7f3e8f9a8f2a61c126fe3
Ruslan <berkutruslan1@gmail.com>
2021-01-16 19:10:21 +0200

    little fixes in locators, in some cases changed xpath to full xpath

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -11,7 +11,7 @@ class DemoMainPageLocators:
     CART_BUTTON = (By.XPATH, "//a[@id='cartur']")
     LOGIN_BUTTON = (By.XPATH, "//a[.='Log in']")
     SIGN_UP_BUTTON = (By.XPATH, "//a[.='Sign up']")
-    PREVIOUS_ADVERTISING_IMAGE = (By.XPATH, "span[@class='carousel-control-prev-icon']")
+    PREVIOUS_ADVERTISING_IMAGE = (By.XPATH, "//span[@class='carousel-control-prev-icon']")
     NEXT_ADVERTISING_IMAGE = (By.XPATH, "//span[@class='carousel-control-next-icon']")
 
 
@@ -27,8 +27,8 @@ class DemoCategoryLocators:
 @dataclass
 class PhoneDemoProductLocators:
     SAMSUNG_GALAXY_S6 = (By.XPATH, "//a[.='Samsung galaxy s6']")
-    NOKIA_LUMIA_1520 = (By.XPATH, "//a[.='Nokia lumia 1520']']")
-    NEXUS_6 = (By.XPATH, "//a[.='Nexus 6']']")
+    NOKIA_LUMIA_1520 = (By.XPATH, "//a[.='Nokia lumia 1520']")
+    NEXUS_6 = (By.XPATH, "//a[.='Nexus 6']")
     SAMSUNG_GALAXY_S7 = (By.XPATH, "//a[.='Samsung galaxy s7']")
     IPHONE_6_32GB = (By.XPATH, "//a[.='Iphone 6 32gb']")
     SONY_XPERIA = (By.XPATH, "//a[.='Sony xperia z5']")
@@ -39,13 +39,13 @@ class PhoneDemoProductLocators:
 class LaptopDemoProductLocators:
     SONY_VAIO_I5 = (By.XPATH, "//a[.='Sony vaio i5']")
     SONY_VAIO_i7 = (By.XPATH, "//a[contains(.,'Sony vaio i7')]")
-    MACBOOK_PRO = (By.XPATH, "//a[.='MacBook Pro']")
-    MACBOOK_AIR = (By.XPATH, "//a[.='MacBook air']")
-    DELL_I7 = (By.XPATH, "//a[.='Dell i7 8gb']")
-    DELL_2017 = (By.XPATH, "//a[.='2017 Dell 15.6 Inch']")
+    MACBOOK_PRO = (By.XPATH, "/html/body/div[5]/div/div[2]/div/div[6]/div/div/h4/a")
+    MACBOOK_AIR = (By.XPATH, "/html/body/div[5]/div/div[2]/div/div[3]/div/div/h4/a")
+    DELL_I7 = (By.XPATH, "/html/body/div[5]/div/div[2]/div/div[4]/div/div/h4/a")
+    DELL_2017 = (By.XPATH, "/html/body/div[5]/div/div[2]/div/div[5]/div/div/h4/a")
 
 
 @dataclass
 class MonitorsDemoProductLocators:
-    APPLE_MONITOR_24 = (By.XPATH, "//a[.='Apple monitor 24']")
-    ASUS_FULL_HD = (By.XPATH, "//a[.='ASUS Full HD']")
+    APPLE_MONITOR_24 = (By.XPATH, "/html/body/div[5]/div/div[2]/div/div[1]/div/div/h4/a")
+    ASUS_FULL_HD = (By.XPATH, "/html/body/div[5]/div/div[2]/div/div[2]/div/div/h4/a")
