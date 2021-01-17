0863f39eab87d97bcc06f94031e52065d7d0412c
Artem <artem@omo.systems>
2021-01-17 22:24:43 +0200

    fix

--- conftest.py
+++ conftest.py
@@ -1,12 +1,11 @@
 import pytest
 import chromedriver_binary
-from RandomWordGenerator import RandomWord
 from selenium import webdriver
 
 
 @pytest.fixture
 def browser():
-    b = webdriver.Chrome(executable_path=chromedriver_binary.chromedriver_filename)
+    b = webdriver.Chrome()
     b.get("https://www.demoblaze.com/")
     b.implicitly_wait(5)
     yield b
