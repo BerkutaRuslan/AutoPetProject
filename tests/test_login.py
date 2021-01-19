1552d6f989a91bb16798931327c3885c8398d5d2
Ruslan <berkutruslan1@gmail.com>
2021-01-19 14:59:15 +0200

    minor fixes, added functions from utils

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,7 +5,8 @@ from selenium.webdriver.common.by import By
 from pages.main_page import DemoMainPage
 from pages.login_page import LoginPage
 from pages.signup_page import SignUpPage
-from time import sleep
+
+from utils import wait_for_alert_to_appear, wait_for_element_be_located
 
 
 def test_login_into_account(browser, user):
@@ -13,11 +14,11 @@ def test_login_into_account(browser, user):
     main_page.navigate_to_signup_page()
 
     SignUpPage(browser).enter_valid_credentials(login=user["login"], password=user["password"])
-    WebDriverWait(browser, 10).until(EC.alert_is_present())
+    wait_for_alert_to_appear(browser, 5)
     alert = browser.switch_to.alert
     alert.accept()
 
     main_page.navigate_lo_login_page()
     LoginPage(browser).enter_valid_credentials(login=user["login"], password=user["password"])
-    WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.ID, "nameofuser")))
+    wait_for_element_be_located(browser, 10, 'nameofuser')
     assert DemoMainPage(browser, registered=True).welcome_label.text == f"Welcome {user['login']}"
