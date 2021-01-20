900d885c8f5e4dce4371556ed8265d0971a4fb85
Ruslan <berkutruslan1@gmail.com>
2021-01-20 19:26:04 +0200

    added two new functions, test creds to login and order info

--- tests/test_login.py
+++ tests/test_login.py
@@ -6,7 +6,7 @@ from pages.main_page import DemoMainPage
 from pages.login_page import LoginPage
 from pages.signup_page import SignUpPage
 
-from utils import wait_for_alert_to_appear, wait_for_element_be_located
+from utils import wait_for_alert_and_accept, wait_for_element_be_located
 
 
 def test_login_into_account(browser, user):
@@ -14,7 +14,7 @@ def test_login_into_account(browser, user):
     main_page.navigate_to_signup_page()
 
     SignUpPage(browser).enter_valid_credentials(login=user["login"], password=user["password"])
-    wait_for_alert_to_appear(browser, 5)
+    wait_for_alert_and_accept(browser, 5)
     alert = browser.switch_to.alert
     alert.accept()
 
