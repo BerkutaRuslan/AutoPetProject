ee5852e6d240e5f7eba6ad43693e24012d3b06bb
Ruslan <berkutruslan1@gmail.com>
2021-01-20 19:27:46 +0200

    updated alert function, now it accepts alert message

--- utils.py
+++ utils.py
@@ -3,8 +3,11 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 
 
-def wait_for_alert_to_appear(browser, time_to_wait):
-    return WebDriverWait(browser, time_to_wait).until(EC.alert_is_present())
+def wait_for_alert_and_accept(browser, time_to_wait):
+    WebDriverWait(browser, time_to_wait).until(EC.alert_is_present())
+    alert = browser.switch_to.alert
+    alert.accept()
+    return 'accepted'
 
 
 def wait_for_element_be_located(browser, time_to_wait, element_id):
